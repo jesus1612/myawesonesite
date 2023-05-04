6346773b5f43b6cacf7ddafa4c31a9fb6c4b6b91
Genaro <gjmg1612@gmail.com>
2023-05-04 17:53:20 -0600

    ACL enabled for new versions

--- src/app/page.js
+++ src/app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            ACL enable in new versions <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
